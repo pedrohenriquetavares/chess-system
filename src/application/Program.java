8b36d7f8cab3fdeca72311cb11448eea55ca270d
Pedro Tavares <phtavares2000@gmail.com>
2024-10-15 13:49:38 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,6 @@ public class Program {
     public static void main(String[] args) {
 
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+            UI.printBoard(chessMatch.getPieces());
     }
 }
